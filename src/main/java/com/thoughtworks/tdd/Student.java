7158a7a4fe75706a33d91beb49171c534bdd3dd5
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-12 20:04:23 +0800

    refactor:refactor function number reduce calculating amount

--- src/main/java/com/thoughtworks/tdd/Student.java
+++ src/main/java/com/thoughtworks/tdd/Student.java
@@ -3,12 +3,15 @@ package com.thoughtworks.tdd;
 public class Student {
 
     public static String number(int i) {
-        if(i%3==0&&i%5==0&&i%7==0)
+        if(i%105==0)
             return "FizzBuzzWhizz";
-        else if(i%3==0&&i%5==0){
+        else if(i%15==0){
             return "FizzBuzz";
         }
-        else if(i%5==0&&i%7==0){
+        else if(i%21==0){
+            return "FizzWhizz";
+        }
+        else if(i%35==0){
             return "BuzzWhizz";
         }
         else if(i%3==0)
